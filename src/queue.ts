e264e0f8d3298d600148c73dacb58aa218443954
Hossein Mayboudi <mh.mayboudi@nizek.com>
2024-07-17 11:40:45 +0330

    feat: add queue
    issue #15

---
+++ src/queue.ts
@@ -0,0 +1,66 @@
+import {Deferred, Effect, Fiber, Queue, Array} from 'effect';
+
+const performWork = (value: number) =>
+  Effect.log(`Consuming value: '${value}'`).pipe(
+    Effect.delay('20 millis'),
+    Effect.as(`Processed value: '${value}'`),
+    Effect.onInterrupt(() => Effect.log('Work was interrupted!'))
+  );
+
+const program = Effect.gen(function* (_) {
+  const queue = yield* _(
+    Queue.unbounded<[number, Deferred.Deferred<string>]>()
+  );
+
+  const produceWork = (value: number): Effect.Effect<string> =>
+    Deferred.make<string>().pipe(
+      Effect.flatMap(deferred =>
+        Queue.offer(queue, [value, deferred]).pipe(
+          Effect.zipRight(Deferred.await(deferred)),
+          Effect.onInterrupt(() =>
+            Effect.log('Production of work was interrupted!').pipe(
+              Effect.zipRight(Deferred.interrupt(deferred))
+            )
+          )
+        )
+      )
+    );
+
+  const consumeWork: Effect.Effect<void> = Queue.take(queue).pipe(
+    Effect.flatMap(([value, deferred]) =>
+      Effect.if(Deferred.isDone(deferred), {
+        onTrue: () => Effect.void,
+        onFalse: () =>
+          performWork(value).pipe(
+            Effect.zipLeft(Effect.sleep('1 seconds')),
+            Effect.exit,
+            Effect.flatMap(result => Deferred.complete(deferred, result)),
+            Effect.race(Deferred.await(deferred)),
+            Effect.fork
+          ),
+      })
+    ),
+    Effect.forever
+  );
+
+  const fiber = yield* _(
+    consumeWork,
+    Effect.annotateLogs('role', 'consumer'),
+    Effect.fork
+  );
+
+  yield* _(
+    Effect.forEach(
+      Array.range(0, 10),
+      value =>
+        produceWork(value).pipe(Effect.flatMap(result => Effect.log(result))),
+      {concurrency: 'unbounded'}
+    ),
+    Effect.zipRight(produceWork(11).pipe(Effect.timeout('10 millis'))),
+    Effect.annotateLogs('role', 'producer')
+  );
+
+  yield* _(Fiber.join(fiber));
+});
+
+program.pipe(Effect.tapErrorCause(Effect.logError), Effect.runFork);
